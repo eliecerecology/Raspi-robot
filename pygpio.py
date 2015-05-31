08a9754b0ca881c366ed18ae941392b5a053112d
Eliecer <eliecer@localhost.com>
2015-05-31 17:54:57 +0300

    rotation

--- pygpio.py
+++ pygpio.py
@@ -57,8 +57,8 @@ def stop(tf):
 pygame.init()
 
 
-display_width = 800
-display_height = 600
+display_width = 1000
+display_height = 1000
 
 black = (0,0,0) # no color
 white = (255,255,255)
@@ -67,33 +67,43 @@ red = (255,0,0)
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('SuavisGame')
 clock = pygame.time.Clock()
-
 robImg = pygame.image.load('Robot2.png')
 
+#def rotate(a):
+   # where = 200,200
+   # blitrobot= screen.blit(surf, where)
+   # surf =  pygame.Surface((100, 100))
+   # surf.fill((255, 255, 255))
+ #   robImg = pygame.transform.rotate(robImg, a)
+    
 #colors
 white = (255,255,255)
 black = (0,0,0)
 red =(255,0,0)
 green = (0,255,0)
-blue = (0,0,255)
+#blue = (0,0,255)
 
 #so where to display or location
-def robot(x,y):
-    gameDisplay.blit(robImg,(x,y))
+
+def robot(a,x,y):
+    robImg1 = pygame.transform.rotate(robImg,a)
+    gameDisplay.blit(robImg1,(x,y))
 
 def game_loop():
     init()
-    sleep_time = 5
-
-    x = (display_width *0.800) #0.45
-    y = (display_height *0.600) # 0.40
+    sleep_time = 1
+       
+    x = (display_width / 4) #0.45
+    y = (display_height / 4) # 0.40
 
     x_change = 0
-
+    y_change = 0
     crashed = False
+    a = 0
+    a_change = 0
 
     while not crashed:
-
+        
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 crashed = True
@@ -101,30 +111,49 @@ def game_loop():
              #aca empieza el control
             if event.type ==  pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x_change = -sleep_time
-                    forward(sleep_time)
-                    GPIO.cleanup()
-                elif event.key == pygame.K_RIGHT:
+                    #x_change = -5
+                    a_change = 20
+                    x_change = -20
+
+                if event.key == pygame.K_RIGHT:  
+                    a_change = 0
                     x_change = 5
+                    right(0.3)
+                    forward(0.3)
+                    
+                if event.key == pygame.K_UP:
+                    a_change = 0
+                    y_change = -5
+                    forward(0.3)
+    
+                elif event.key == pygame.K_DOWN:
+                    a_change = 0
+                    y_change = 5
+                    reverse(0.3)
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    a_change = 0
                     x_change = 0
-            '''             
-            if event.type ==  pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    x_change = -5
-                elif event.key == pygame.K_DOWN:
-                    x_change = 5
-             '''
-                   
+                    stop(0.2)
+                    init()
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    a_change = 0
+                    y_change = 0
+                    stop(0.2)
+                    init()      
+        
         #    print(event)
-        x += x_change      
+        x += x_change
+        y += y_change      
+        a += a_change
         gameDisplay.fill(white)    
-        robot(x,y) #position of the car    
-        pygame.draw.rect(gameDisplay, black,(display_width*0,display_height*0,50,50))
-        pygame.draw.rect(gameDisplay, green,(display_width*0.5,display_height*0.5,50,50))
-
+        #rotate(a)
+        robot(a, x, y) #position of the car
+    
+        #robImg = pygame.transform.rotate(robImg, 90)
+        #pygame.draw.rect(gameDisplay, black,(display_width*0,display_height*0,50,50))
+        #####pygame.draw.rect(gameDisplay, green,(display_width*0.5,display_height*0.5,50,50))
         pygame.display.flip() #pygame.display.flip()
         clock.tick(60) #69 frams per second
 game_loop()
