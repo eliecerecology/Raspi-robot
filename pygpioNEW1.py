9a13d81223c0b872f8b48e7f7ffa25ac9c2491bb
Eliecer <eliecer@localhost.com>
2015-07-28 21:47:48 +0300

    R files

--- pygpioNEW1.py
+++ pygpioNEW1.py
@@ -3,9 +3,44 @@ import pygame
 import time
 import sys
 from pygame.locals import *
-import RPi.GPIO as GPIO
-
+##import RPi.GPIO as GPIO
+'''
 pwm = {}
+
+RIGHT = 7
+RIGHT_BACK = 11
+LEFT_BACK = 13
+LEFT = 15
+pins = [RIGHT, LEFT, LEFT_BACK, RIGHT_BACK]
+
+def init():
+    GPIO.setmode(GPIO.BOARD)
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, False)
+        pwm[pin] = GPIO.PWM(pin, 100)
+
+def off():
+    for pin in pins:
+        pwm[pin].stop()
+
+def move(pins=[], speed=50):
+    for pin in pins:
+        realspeed = speed
+        if pin in [RIGHT, RIGHT_BACK]:
+            realspeed = speed * 0.50
+        pwm[pin].start(realspeed)
+
+def left(tf):
+    move([LEFT])
+    time.sleep(tf)
+    
+def right(tf):
+    move([RIGHT])
+    time.sleep(tf)
+    
+def forward(tf):
+    move([RIGHT, LEFT])pwm = {}
 
 RIGHT = 7
 RIGHT_BACK = 11
@@ -49,6 +84,15 @@ def fast(tf):
 def reverse(tf):
     move([RIGHT_BACK, LEFT_BACK])
     time.sleep(tf)
+    time.sleep(tf)
+    
+def fast(tf):
+    move([RIGHT, LEFT], 100)
+    time.sleep(tf)
+def reverse(tf):
+    move([RIGHT_BACK, LEFT_BACK])
+    time.sleep(tf)
+'''    
 pygame.init()
 
 white = (255,255,255)
@@ -73,6 +117,10 @@ clock = pygame.time.Clock()
 #pygame.draw.rect(setDisplay, purple,(100,100,200,100))
 img1 = pygame.image.load('arrow.png')
 img = pygame.transform.scale(img1, (60, 160))
+
+def rotate():
+    pygame.transform.rotate(img, 90)
+    
 #def imagen(x,y):
 #setDisplay.blit(img,(x,y))
 x = 50
@@ -80,7 +128,7 @@ y = 50
 #pygame.draw.polygon(setDisplay, yellow,((80,60),(100,100),(80,120),(80,60)))
 x_change = 0
 y_change = 0
-init()
+#init()
 
 while True:
     for event in pygame.event.get():
@@ -90,12 +138,16 @@ while True:
             sys.exit()
             
         if event.type ==  pygame.KEYDOWN:
+
             if event.key == pygame.K_SPACE:
-	       	img = pygame.transform.rotate(img,10)
-	       	left(0.1)
-	       	
+                rotate() 
+                #img = pygame.transform.rotate(img,90)
+	       	#left(0.1)
+	       	#if rotate() == True:
+                 #   print "yeahh"
+                    
             if event.key == pygame.K_LEFT:
-                img = pygame.transform.rotate(img,10)
+                #img = pygame.transform.rotate(img,10)
                 x_change = -5
                 
                 
@@ -111,15 +163,15 @@ while True:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 y_change = 0
-                off(0.2)
-                init()
+                ##off(0.2)
+                ##init()
 
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 x_change = 0
-                off(0.2)
-                init()
+                ##off(0.2)
+                ##init()
 
         #print x,y  
         #print(event)
